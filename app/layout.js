4cfbf8309aff920b7a8d7d2b356d0b44edab22f0
Ankit Tiwari <ankittiwari56@users.noreply.github.com>
2026-06-11 11:15:56 +0530

    Add Google site verification

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
 import "./globals.css";
 import Navbar from "../components/Navbar";
 
+export const metadata = {
+  verification: {
+    google: "iJSpImcsjKHNovHphl2Dlw_L91n8ki-bAsD_1qXeir0",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
